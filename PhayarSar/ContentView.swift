b6ed8796e896c78bb335af64dcec6196880c83d1
Kyaw Zay Ya Lin Tun <kyaw.codes@gmail.com>
2023-12-03 20:35:45 +0630

    chore: initial set up

---
+++ PhayarSar/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PhayarSar
+//
+//  Created by Kyaw Zay Ya Lin Tun on 03/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhayarSar/PhayarSarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhayarSarApp.swift
+//  PhayarSar
+//
+//  Created by Kyaw Zay Ya Lin Tun on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhayarSarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
