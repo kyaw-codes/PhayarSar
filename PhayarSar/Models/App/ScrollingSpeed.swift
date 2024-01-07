2089954d63a20ada470a14fe2a4a439d86fdf59f
Kyaw <kyaw.codes@gmail.com>
2024-01-07 18:34:53 +0630

    chore: added custom nav bar

---
+++ PhayarSar/Models/App/ScrollingSpeed.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollingSpeed.swift
+//  PhayarSar
+//
+//  Created by Kyaw Zay Ya Lin Tun on 07/01/2024.
+//
+
+import SwiftUI
+
+struct ScrollingSpeed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollingSpeed()
+}
