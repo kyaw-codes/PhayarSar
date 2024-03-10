1c3ff20cdc2660d8ce004c55e34aba9e77b2d761
Kyaw <kyaw.codes@gmail.com>
2024-03-10 21:29:13 +0630

    chore: clean up code for WorshipPlanPrayingScreen

--- PhayarSar/Features/WorshipPlanPraying/WorshipPlanPrayingScreen.swift
+++ PhayarSar/Features/WorshipPlanPraying/WorshipPlanPrayingScreen.swift
@@ -18,114 +18,146 @@ struct WorshipPlanPrayingScreen: View {
   
   var body: some View {
     VStack(spacing: 0){
-      Rectangle()
-        .fill(Color.navBar)
-        .frame(height: safeAreaInsets.top)
+      SafeAreaFillerView()
 
-      TabView(selection: $currentPrayerId) {
-        ForEach(worshipPlan.selectedPrayers) { prayer in
-          CommonPrayerScreen(model: prayer, worshipPlanName: worshipPlan.planName)
-            .tag(prayer.id)
-        }
-      }
-      .tabViewStyle(.page(indexDisplayMode: .never))
-      .mask {
-        CustomCornerView(corners: [.bottomLeft, .bottomRight], radius: 30)
-      }
-      .overlay {
-        CustomCornerView(corners: [.bottomLeft, .bottomRight], radius: 30)
-          .stroke(
-            LinearGradient(
-              colors: [
-                .clear,
-                .secondary.opacity(0.2),
-                .secondary.opacity(0.2),
-                .secondary.opacity(0.2)
-              ],
-              startPoint: .top,
-              endPoint: .bottom
-            ),
-            lineWidth: 1.5
-          )
-      }
+      CommonPrayerTabView()
       
       Rectangle()
         .fill(Color.cardBg)
         .overlay(alignment: .top) {
-          VStack(spacing: 4) {
-            if preferences.appLang == .Mm {
-              LocalizedText(
-                .x_of_y,
-                args: [localizeNumber(preferences.appLang, str: "\(index(of: currentPrayerId) + 1)"), localizeNumber(preferences.appLang, str: "\(worshipPlan.selectedPrayers.count)")]
-              )
-              .foregroundColor(.secondary)
-              .font(.qsB(12))
-            } else {
-              AnimateNumberText(
-                font: .qsB(12),
-                value: $currentIndex,
-                textColor: .init(get: { .secondary }, set: { _ in }),
-                stringFormatter: "%@ of \(worshipPlan.selectedPrayers.count)"
-              )
-            }
-            
-            Text(worshipPlan.selectedPrayers[index(of: currentPrayerId)].title)
-              .font(.qsB(14))
-              .foregroundColor(preferences.accentColor.color)
-          }
-          .padding(.top, 12)
+          CurrentPrayerTitleView()
         }
         .overlay {
-          HStack {
-            Button {
-              guard worshipPlan.selectedPrayers.first?.id != currentPrayerId else { return }
-              let index = index(of: currentPrayerId)
-              withAnimation {
-                currentPrayerId = worshipPlan.selectedPrayers[index - 1].id
-              }
-            } label: {
-              Image(systemName: "arrow.left")
-                .font(.headline)
-                .padding(8)
-                .foregroundColor(preferences.accentColor.color)
-                .background(
-                  Circle()
-                    .fill(.white)
-                )
-            }
-            
-            Spacer()
-            
-            Button {
-              guard worshipPlan.selectedPrayers.last?.id != currentPrayerId else { return }
-              let index = index(of: currentPrayerId)
-              withAnimation {
-                currentPrayerId = worshipPlan.selectedPrayers[index + 1].id
-              }
-            } label: {
-              Image(systemName: "arrow.right")
-                .font(.headline)
-                .padding(8)
-                .foregroundColor(preferences.accentColor.color)
-                .background(
-                  Circle()
-                    .fill(.white)
-                )
-            }
-          }
-          .padding(.horizontal, 40)
-          .onChange(of: currentPrayerId) { id in
-            currentIndex = Double(index(of: id))
-          }
+          NextPreviousControlView()
         }
         .frame(height: 48 + safeAreaInsets.bottom)
     }
     .background(Color.cardBg)
     .ignoresSafeArea()
+    .onAppear {
+      currentPrayerId = worshipPlan.selectedPrayers.first?.id ?? ""
+    }
+    .onChange(of: currentPrayerId) { id in
+      currentIndex = Double(index(of: id))
+    }
   }
   
   private func index(of currentPrayerId: String) -> Int {
     worshipPlan.selectedPrayers.firstIndex(where: { $0.id == currentPrayerId }) ?? 0
+  }
+  
+  @ViewBuilder
+  func SafeAreaFillerView() -> some View {
+    Rectangle()
+      .fill(Color.navBar)
+      .frame(height: safeAreaInsets.top)
+  }
+  
+  @ViewBuilder
+  func SilverBorderView() -> some View {
+    CustomCornerView(corners: [.bottomLeft, .bottomRight], radius: 30)
+      .stroke(
+        LinearGradient(
+          colors: [
+            .clear,
+            .secondary.opacity(0.2),
+            .secondary.opacity(0.2),
+            .secondary.opacity(0.2)
+          ],
+          startPoint: .top,
+          endPoint: .bottom
+        ),
+        lineWidth: 1.5
+      )
+  }
+  
+  @ViewBuilder
+  func CommonPrayerTabView() -> some View {
+    TabView(selection: $currentPrayerId) {
+      ForEach(worshipPlan.selectedPrayers) { prayer in
+        CommonPrayerScreen(model: prayer, worshipPlanName: worshipPlan.planName)
+          .tag(prayer.id)
+      }
+    }
+    .tabViewStyle(.page(indexDisplayMode: .never))
+    .mask { CustomCornerView(corners: [.bottomLeft, .bottomRight], radius: 30) }
+    .overlay { SilverBorderView() }
+  }
+  
+  @ViewBuilder
+  func CurrentPrayerTitleView() -> some View {
+    VStack(spacing: 4) {
+      if preferences.appLang == .Mm {
+        LocalizedText(
+          .x_of_y,
+          args: [localizeNumber(preferences.appLang, str: "\(index(of: currentPrayerId) + 1)"), localizeNumber(preferences.appLang, str: "\(worshipPlan.selectedPrayers.count)")]
+        )
+        .foregroundColor(.secondary)
+        .font(.qsB(12))
+      } else {
+        AnimateNumberText(
+          font: .qsB(12),
+          value: $currentIndex,
+          textColor: .init(get: { .secondary }, set: { _ in }),
+          stringFormatter: "%@ of \(worshipPlan.selectedPrayers.count)"
+        )
+      }
+      
+      Text(worshipPlan.selectedPrayers[index(of: currentPrayerId)].title)
+        .font(.qsB(14))
+        .foregroundColor(preferences.accentColor.color)
+    }
+    .padding(.top, 12)
+  }
+  
+  @ViewBuilder
+  func NextPreviousControlView() -> some View {
+    HStack {
+      PreviousPrayerControlView()
+      Spacer()
+      NextPrayerControlView()
+    }
+    .padding(.horizontal, 40)
+  }
+  
+  @ViewBuilder
+  func PreviousPrayerControlView() -> some View {
+    Button {
+      guard worshipPlan.selectedPrayers.first?.id != currentPrayerId else { return }
+      let index = index(of: currentPrayerId)
+      withAnimation {
+        currentPrayerId = worshipPlan.selectedPrayers[index - 1].id
+      }
+    } label: {
+      Image(systemName: "arrow.left")
+        .font(.headline)
+        .padding(8)
+        .foregroundColor(preferences.accentColor.color)
+        .background(
+          Circle()
+            .fill(.white)
+        )
+    }
+  }
+  
+  @ViewBuilder
+  func NextPrayerControlView() -> some View {
+    Button {
+      guard worshipPlan.selectedPrayers.last?.id != currentPrayerId else { return }
+      let index = index(of: currentPrayerId)
+      withAnimation {
+        currentPrayerId = worshipPlan.selectedPrayers[index + 1].id
+      }
+    } label: {
+      Image(systemName: "arrow.right")
+        .font(.headline)
+        .padding(8)
+        .foregroundColor(preferences.accentColor.color)
+        .background(
+          Circle()
+            .fill(.white)
+        )
+    }
   }
 }
 
