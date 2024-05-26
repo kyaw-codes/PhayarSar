54157d27079bddee0944fd6b01de1628d18b58c8
Kyaw Monkey <kyaw.codes@gmail.com>
2024-05-26 14:16:22 +0700

    chore: set up FCM for push-noti

--- PhayarSar/PhayarSarApp.swift
+++ PhayarSar/PhayarSarApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import UserNotifications
 import FirebaseCore
+import FirebaseMessaging
 
 var langDict: [String: [String: String]] = [:]
 
@@ -84,10 +85,30 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
           print(error)
         }
       }
-    UNUserNotificationCenter.current().delegate = self
     
     // Set up firebase
     FirebaseApp.configure()
+    
+    // Set up push-notification
+    UNUserNotificationCenter.current().delegate = self
+    
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in }
+    )
+
+    application.registerForRemoteNotifications()
+    
+    Messaging.messaging().delegate = self
+    
+    Messaging.messaging().token { token, error in
+      if let error = error {
+        print("Error fetching FCM registration token: \(error)")
+      } else if let token = token {
+        print("FCM registration token: \(token)")
+      }
+    }
     
     return true
   }
@@ -95,4 +116,25 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
     completionHandler([.banner, .sound])
   }
+  
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: .fcmToken,
+      object: nil,
+      userInfo: dataDict
+    )
+  }
+}
+
+extension Notification.Name {
+  static let fcmToken: Self = .init(rawValue: "FCMToken")
 }
