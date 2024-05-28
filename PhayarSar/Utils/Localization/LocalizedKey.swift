652392d423f495cb527332b108abf4e7740864d9
Kyaw Monkey <kyaw.codes@gmail.com>
2024-05-28 22:42:22 +0700

    chore: localized pull to refresh text

--- PhayarSar/Utils/Localization/LocalizedKey.swift
+++ PhayarSar/Utils/Localization/LocalizedKey.swift
@@ -173,6 +173,7 @@ enum LocalizedKey: String {
   case update_now
   case version_x_is_available
   case version_x_is_available_desc
+  case pull_to_add_new_worship_plan
   
   func localize(_ lang: AppLanguage) -> String? {
     let dict = langDict[self.rawValue]
